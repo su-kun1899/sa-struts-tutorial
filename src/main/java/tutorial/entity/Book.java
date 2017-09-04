a41b7dd2280f8d2011b2df04049f95f7f72f3c3b
su-kun1899 <higedrum.coz@gmail.com>
2017-09-04 12:37:44 +0900

    Add book entity

---
+++ src/main/java/tutorial/entity/Book.java
@@ -0,0 +1,20 @@
+package tutorial.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author su-kun1899
+ */
+@Entity
+@Table(name = "book1")
+public class Book {
+    @Id
+    @Column
+    public long isbn;
+
+    @Column
+    public String title;
+}
